f32a160fbfdbb6b43bca19b1968cf7ca9c1276a1
Sarah-Albalawi <s.albalawi@outlook.com>
2022-10-28 13:16:19 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,42 @@
 // Code your solutions in this file
+/*for (let age = 30; age < 40; age++) {
+    console.log(`I'm ${age} years old. Happy birthday to me!`);
+    debugger;
+  }
+
+const gifts = ["teddy bear", "drone", "doll"];
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+*/
+
+;
+let message=[];
+const name =['Guadalupe', 'Ollie', 'Aki'] ;
+function writeCards (name){
+    for(let i=0; i < name.length ; i++){
+       message = [`Thank you, ${name[i]}, for the wonderful surprise gift!`];
+       console.log(message);
+        
+        
+    }
+ return message;
+
+}
+writeCards();
+
+
+
+function countDown(){
+for (let countDown = 10; countDown >= 0; countDown--) {
+    console.log(countDown);
+  }
+}
